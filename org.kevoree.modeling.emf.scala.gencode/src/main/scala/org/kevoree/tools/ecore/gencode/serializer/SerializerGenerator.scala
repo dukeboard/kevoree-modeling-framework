6b046c682c7f1b082baaf08a682401d173ab855f
François Fouquet <fouquet.f@gmail.com>
2012-04-05 14:24:18 +0200

    improve serialize performance

--- org.kevoree.modeling.emf.scala.gencode/src/main/scala/org/kevoree/tools/ecore/gencode/serializer/SerializerGenerator.scala
+++ org.kevoree.modeling.emf.scala.gencode/src/main/scala/org/kevoree/tools/ecore/gencode/serializer/SerializerGenerator.scala
@@ -49,6 +49,23 @@
  * Fouquet Francois
  * Nain Gregory
  */
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors:
+ * Fouquet Francois
+ * Nain Gregory
+ */
 
 
 package org.kevoree.tools.ecore.gencode.serializer
@@ -385,7 +402,16 @@ buffer.print(stringListSubSerializers.mkString(" extends ", " with ", " "))
                   buffer.println("\t\t\t\t\t\t}")
                 }
                 case 1 => {
-                  buffer.println("\t\t\t\t\t\tsubAtts= subAtts.append(new scala.xml.UnprefixedAttribute(\"" + ref.getName + "\",addrs.get(selfObject." + getGetter(ref.getName) + ").getOrElse{\"non contained reference\"},scala.xml.Null))")
+
+
+                  if (ref.getEOpposite != null) {
+                    if (ref.getEOpposite.getUpperBound != -1) {
+                      buffer.println("\t\t\t\t\t\tsubAtts= subAtts.append(new scala.xml.UnprefixedAttribute(\"" + ref.getName + "\",addrs.get(selfObject." + getGetter(ref.getName) + ").getOrElse{\"non contained reference\"},scala.xml.Null))")
+                    }
+                  } else {
+                    buffer.println("\t\t\t\t\t\tsubAtts= subAtts.append(new scala.xml.UnprefixedAttribute(\"" + ref.getName + "\",addrs.get(selfObject." + getGetter(ref.getName) + ").getOrElse{\"non contained reference\"},scala.xml.Null))")
+                  }
+
                 }
               }
 
