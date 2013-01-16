b56aca456b35ab1d32d84045891d26e0aa682bbe
Grégory NAIN <gregory.nain@gmail.com>
2013-01-16 10:21:15 +0100

    Fixing closures

--- org.kevoree.modeling.emf.kotlin.gencode/src/main/scala/org/kevoree/tools/ecore/kotlin/gencode/loader/xml/ContextGenerator.scala
+++ org.kevoree.modeling.emf.kotlin.gencode/src/main/scala/org/kevoree/tools/ecore/kotlin/gencode/loader/xml/ContextGenerator.scala
@@ -54,7 +54,7 @@ class ContextGenerator(ctx:GenerationContext, genDir: String, genPackage: String
     pr.println()
     pr.println("\t\tval elementsCount : java.util.HashMap<String, Int> = java.util.HashMap<String, Int>()")
     pr.println()
-    pr.println("\t\tval resolvers : MutableList<() -> Any> = java.util.ArrayList<() -> Any>()")
+    pr.println("\t\tval resolvers : MutableList<() -> Unit> = java.util.ArrayList<() -> Unit>()")
     pr.println()
     pr.println("\t\tval stats : java.util.HashMap<String, Int> = java.util.HashMap<String, Int>()")
     pr.println()
