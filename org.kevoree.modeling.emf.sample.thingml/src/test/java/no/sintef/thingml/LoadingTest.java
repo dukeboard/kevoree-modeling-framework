cc58898ec36a72e73dd1328d4868ae0e430c0b13
brice-morin <brice.morin1@gmail.com>
2013-01-31 16:11:46 +0100

    update tests

--- org.kevoree.modeling.emf.sample.thingml/src/test/java/no/sintef/thingml/LoadingTest.java
+++ org.kevoree.modeling.emf.sample.thingml/src/test/java/no/sintef/thingml/LoadingTest.java
@@ -15,10 +15,29 @@
  * 	Fouquet Francois
  * 	Nain Gregory
  */
-package no.sintef.thingml;/*
-* Author : Gregory Nain (developer.name@uni.lu)
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors:
+ * 	Fouquet Francois
+ * 	Nain Gregory
+ *      Brice Morin
+ */
+package no.sintef.thingml;
+/*
 * Date : 29/01/13
-* (c) 2013 University of Luxembourg – Interdisciplinary Centre for Security Reliability and Trust (SnT)
+* (c) 2013 University of Luxembourg & Interdisciplinary Centre for Security Reliability and Trust (SnT)
+* (c) 2013 SINTEF IKT
 * All rights reserved
 */
 
@@ -44,6 +63,34 @@ public class LoadingTest {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
+    
+    @Test
+    public void loadingLightFollower() {
+
+        try {
+
+            ModelLoader loader = new ModelLoader();
+            loader.loadModelFromPath(new File(getClass().getClassLoader().getResource("LightFollower.xmi").toURI()));
+
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+    
+    @Test
+    public void loadingBCMS() {
+
+        try {
+
+            ModelLoader loader = new ModelLoader();
+            loader.loadModelFromPath(new File(getClass().getClassLoader().getResource("bCMS.xmi").toURI()));
+
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }    
 
 
 }
