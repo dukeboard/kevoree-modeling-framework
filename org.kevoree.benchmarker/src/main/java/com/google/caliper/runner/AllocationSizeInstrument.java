89f945a2ca3a63b7e14ac217ce8e7c074661b8e2
Grégory NAIN <gregory.nain@gmail.com>
2013-02-01 14:21:33 +0100

    Adding Caliper sources

---
+++ org.kevoree.benchmarker/src/main/java/com/google/caliper/runner/AllocationSizeInstrument.java
@@ -0,0 +1,16 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+
+package com.google.caliper.runner;
+
+import com.google.caliper.worker.AllocationSizeWorker;
+import com.google.caliper.worker.Worker;
+
+/**
+ * {@link com.google.caliper.runner.AllocationInstrument} that measures the total number of bytes allocated by the
+ * benchmark method.
+ */
+public class AllocationSizeInstrument extends AllocationInstrument {
+  @Override public Class<? extends Worker> workerClass() {
+    return AllocationSizeWorker.class;
+  }
+}
