71acf9a9f591ec6bc3b130c91a3afb9869bdf68f
Grégory NAIN <gregory.nain@gmail.com>
2013-03-28 12:04:18 +0100

    Update of processor Helper

--- org.kevoree.modeling.kotlin.generator/src/main/scala/org/kevoree/modeling/kotlin/generator/ProcessorHelperClass.scala
+++ org.kevoree.modeling.kotlin.generator/src/main/scala/org/kevoree/modeling/kotlin/generator/ProcessorHelperClass.scala
@@ -15,42 +15,6 @@
  * Fouquet Francois
  * Nain Gregory
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Authors:
- * 	Fouquet Francois
- * 	Nain Gregory
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Authors:
- * Fouquet Francois
- * Nain Gregory
- */
-
-
 package org.kevoree.modeling.kotlin.generator
 
 import java.io.{PrintWriter, File}
@@ -120,6 +84,7 @@ class ProcessorHelperClass {
       case "type" => "`type`"
       case "object" => "`object`"
       case "requires" => "`requires`"
+      case "package" => "`package`"
       case _ => word //throw new UnsupportedOperationException("ProcessorHelper::protectReservedWords::No matching found for word: " + word);null
     }
   }
@@ -198,7 +163,12 @@ class ProcessorHelperClass {
     res
   }
 
-
+  /**
+   * Returns the absolute path of the folder in which to generate classes of a package.
+   * @param ctx The generation context
+   * @param pack the package to be generated
+   * @return The absolute path of the folder where to generate the content of the package.
+   */
   def getPackageGenDir(ctx: GenerationContext, pack: EPackage): String = {
     var modelGenBaseDir = ctx.getRootGenerationDirectory.getAbsolutePath + "/"
     ctx.getPackagePrefix.map(prefix => modelGenBaseDir += prefix.replace(".", "/") + "/")
@@ -206,6 +176,12 @@ class ProcessorHelperClass {
     modelGenBaseDir
   }
 
+  /**
+   * Returns the absolute path of the folder containing implementations made by users for the given package.
+   * @param ctx The generation context
+   * @param pack the package required
+   * @return The absolute path of the folder where to find any implementation made by developers.
+   */
   def getPackageUserDir(ctx: GenerationContext, pack: EPackage): String = {
     if (ctx.getRootUserDirectory != null) {
       var modelGenBaseDir = ctx.getRootUserDirectory.getAbsolutePath + "/"
@@ -374,6 +350,5 @@ class ProcessorHelperClass {
     }
     possibleRoot
   }
-
 
 }
