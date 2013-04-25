8dcd84488cb105837f580f46ea7e52773629776c
Francois Fouquet <fouquet.f@gmail.com>
2013-04-25 18:08:16 +0200

    cleanup

---
+++ samples/org.kevoree.modeling.sample.kevoree/src/test/java/HelloEquals.kt
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 24/04/13
+ * Time: 17:22
+ * To change this template use File | Settings | File Templates.
+ */
+
+class titi {
+    fun equals(similarObj : Any?) : Boolean {
+        println("Yop")
+       return true
+    }
+}
+fun main(args : Array<String>) {
+
+    val t = titi()
+    val t2 = titi()
+
+    t == t2
+
+}
