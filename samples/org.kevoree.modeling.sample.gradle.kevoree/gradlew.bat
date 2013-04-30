4d34ec33896642bb9e64ecaaa69f1e0de5a074e7
Francois Fouquet <fouquet.f@gmail.com>
2013-04-30 21:24:43 +0200

    cleanup

--- samples/org.kevoree.modeling.sample.gradle.kevoree/gradlew.bat
+++ samples/org.kevoree.modeling.sample.gradle.kevoree/gradlew.bat
@@ -1,3 +1,21 @@
+@REM
+@REM Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM http://www.gnu.org/licenses/lgpl-3.0.txt
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+@REM Authors:
+@REM Fouquet Francois
+@REM Nain Gregory
+@REM
+
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
