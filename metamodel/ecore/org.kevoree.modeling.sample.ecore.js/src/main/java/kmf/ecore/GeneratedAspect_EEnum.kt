d89ad77fcb14e36a2b576455b5a5d1a913c40332
Francois Fouquet <fouquet.f@gmail.com>
2013-09-02 10:45:19 +0200

    cleanup

---
+++ metamodel/ecore/org.kevoree.modeling.sample.ecore.js/src/main/java/kmf/ecore/GeneratedAspect_EEnum.kt
@@ -0,0 +1,22 @@
+package kmf.ecore;
+import org.kevoree.modeling.api.aspect;
+public aspect trait GeneratedAspect_EEnum : kmf.ecore.EEnum {
+	override fun getEAnnotation(_source:String) : kmf.ecore.EAnnotation {
+		throw Exception("Not implemented yet !");
+	}
+	override fun isInstance(_object:Any) : Boolean {
+		throw Exception("Not implemented yet !");
+	}
+	override fun getClassifierID() : Int {
+		throw Exception("Not implemented yet !");
+	}
+	override fun getEEnumLiteral(_name:String) : kmf.ecore.EEnumLiteral {
+		throw Exception("Not implemented yet !");
+	}
+	override fun getEEnumLiteral(_value:Int) : kmf.ecore.EEnumLiteral {
+		throw Exception("Not implemented yet !");
+	}
+	override fun getEEnumLiteralByLiteral(_literal:String) : kmf.ecore.EEnumLiteral {
+		throw Exception("Not implemented yet !");
+	}
+}
---
+++ metamodel/ecore/org.kevoree.modeling.sample.ecore.js/src/main/java/kmf/ecore/GeneratedAspect_EObject.kt
@@ -0,0 +1,46 @@
+package kmf.ecore;
+import org.kevoree.modeling.api.aspect;
+public aspect trait GeneratedAspect_EObject : kmf.ecore.EObject {
+	override fun eClass() : kmf.ecore.EClass {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eIsProxy() : Boolean {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eResource() : Any {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eContainingFeature() : kmf.ecore.EStructuralFeature {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eContainmentFeature() : kmf.ecore.EReference {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eContents() : List<Any?> {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eAllContents() : MutableIterator<*> {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eCrossReferences() : List<Any?> {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eGet(_feature:kmf.ecore.EStructuralFeature) : Any {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eGet(_feature:kmf.ecore.EStructuralFeature,_resolve:Boolean) : Any {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eSet(_feature:kmf.ecore.EStructuralFeature,_newValue:Any){
+		throw Exception("Not implemented yet !");
+	}
+	override fun eIsSet(_feature:kmf.ecore.EStructuralFeature) : Boolean {
+		throw Exception("Not implemented yet !");
+	}
+	override fun eUnset(_feature:kmf.ecore.EStructuralFeature){
+		throw Exception("Not implemented yet !");
+	}
+	override fun eInvoke(_operation:kmf.ecore.EOperation,_arguments:List<Any?>) : Any {
+		throw Exception("Not implemented yet !");
+	}
+}
