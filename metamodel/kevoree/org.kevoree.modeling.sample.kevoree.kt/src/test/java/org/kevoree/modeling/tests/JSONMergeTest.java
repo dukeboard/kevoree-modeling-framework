706e41abd4fb06c9e53a32685d5169f0470ca69d
Maxime Tricoire <leiko@braindead.fr>
2013-09-04 15:14:00 +0200

    merge test added

--- metamodel/kevoree/org.kevoree.modeling.sample.kevoree.kt/src/test/java/org/kevoree/modeling/tests/JSONMergeTest.java
+++ metamodel/kevoree/org.kevoree.modeling.sample.kevoree.kt/src/test/java/org/kevoree/modeling/tests/JSONMergeTest.java
@@ -3,9 +3,13 @@ package org.kevoree.modeling.tests;
 import org.junit.Assert;
 import org.kevoree.ContainerRoot;
 import org.kevoree.DictionaryValue;
+import org.kevoree.KevoreeFactory;
 import org.kevoree.TypeDefinition;
+import org.kevoree.compare.DefaultModelCompare;
+import org.kevoree.impl.DefaultKevoreeFactory;
 import org.kevoree.loader.JSONModelLoader;
 import org.kevoree.loader.XMIModelLoader;
+import org.kevoree.modeling.api.trace.TraceSequence;
 import org.kevoree.serializer.JSONModelSerializer;
 
 import java.io.FileNotFoundException;
@@ -16,29 +20,29 @@ import java.io.FileNotFoundException;
  * Date: 01/09/13
  * Time: 13:35
  */
-public class JSONLoadTest {
+public class JSONMergeTest {
 
     @org.junit.Test
     public void test() throws FileNotFoundException {
         XMIModelLoader xmiLoader = new XMIModelLoader();
         JSONModelLoader jsonLoader = new JSONModelLoader();
         JSONModelSerializer jsonSerializer = new JSONModelSerializer();
+        DefaultModelCompare compare = new DefaultModelCompare();
+        KevoreeFactory factory = new DefaultKevoreeFactory();
+        ContainerRoot mergeModel = factory.createContainerRoot();
 
         ContainerRoot fakeDomoModel = (ContainerRoot) xmiLoader.loadModelFromStream(this.getClass().getClassLoader().getResourceAsStream("fakeDomo_2.0.4-SNAPSHOT.kev")).get(0);
+
         TypeDefinition fakeConsoleTD = fakeDomoModel.findTypeDefinitionsByID("FakeConsole");
         for (DictionaryValue dv : fakeConsoleTD.getDictionaryType().getDefaultValues()) {
             Assert.assertNotNull("DictionaryValue.getAttribute() shouldn't be null", dv.getAttribute());
         }
 
-        String fakeDomoModelStr = jsonSerializer.serialize(fakeDomoModel);
+        TraceSequence mergeSeq = compare.merge(mergeModel, fakeDomoModel);
+        mergeSeq.applyOn(mergeModel);
 
-        //System.out.println(fakeDomoModelStr);
-
-        //ByteArrayInputStream bais = new ByteArrayInputStream(fakeDomoModelStr.getBytes());
-        ContainerRoot reloadedFakeDomoModel = (ContainerRoot) jsonLoader.loadModelFromString(fakeDomoModelStr).get(0);
-
-        TypeDefinition fakeConsoleTDfromJSON = reloadedFakeDomoModel.findTypeDefinitionsByID("FakeConsole");
-        for (DictionaryValue dv : fakeConsoleTDfromJSON.getDictionaryType().getDefaultValues()) {
+        fakeConsoleTD = mergeModel.findTypeDefinitionsByID("FakeConsole");
+        for (DictionaryValue dv : fakeConsoleTD.getDictionaryType().getDefaultValues()) {
             Assert.assertNotNull("DictionaryValue.getAttribute() shouldn't be null", dv.getAttribute());
         }
     }
