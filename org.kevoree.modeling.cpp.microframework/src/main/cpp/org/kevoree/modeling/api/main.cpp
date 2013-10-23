684c1412afdf72b370f4082946148cccd1f5c2bf
JeD <jedartois@gmail.com>
2013-10-23 15:14:56 +0200

    add tests in main microframework c++

--- org.kevoree.modeling.cpp.microframework/src/main/cpp/org/kevoree/modeling/api/main.cpp
+++ org.kevoree.modeling.cpp.microframework/src/main/cpp/org/kevoree/modeling/api/main.cpp
@@ -2,6 +2,7 @@
 #include "trace/ModelTrace.h"
 #include "trace/DefaultTraceConverter.h"
 #include "json/Lexer.h"
+#include "trace/TraceSequence.h"
 
 #include <iostream>
 #include <fstream>
@@ -9,33 +10,84 @@
 int main(int argc,char **argv){
 
 
-ModelAddTrace *t = new ModelAddTrace("","","","");
+ModelTrace *model_trace = new ModelAddTrace("","nodes","nodes[server-node]","org.kevoree.ContainerNode");
+
+if(model_trace->toString().compare("{ \"traceType\" : 1 , \"src\" : \"\", \"refname\" : \"nodes\", \"previouspath\" : \"nodes[server-node]\"}") == -1){
+	std::cout << "Error ModelAddTrace toString()" << endl;
+}
+/*
+list<ModelTrace*> traces;
+  
+  traces.push_back(model_trace);
+  
+   	for (std::list<ModelTrace*>::iterator iterator = traces.begin(), end = traces.end(); iterator != end; ++iterator) 
+        {
+			ModelTrace * t = *iterator;
+			
+			cout << t->toString() <<endl;
+		}*/
+  
+/* *********************************Test DefaultTraceConverter ************************ */
 DefaultTraceConverter *defaultTraceConvert = new DefaultTraceConverter();
 
 
-ModelAddTrace *t2 = (ModelAddTrace*)defaultTraceConvert->convert(t);
-if(typeid(t) != typeid(t2)){
-		std::cout << "Error DefaultTraceConverter" << endl;
-}
+ModelAddTrace *t2 = (ModelAddTrace*)defaultTraceConvert->convert(model_trace);
+
+delete model_trace;
 
 
-/* Test Lexer */
+/* ********************************************************************* */ 
+
+
+
+/* *********************************Test Lexer ************************ */
 ifstream myfile;
  myfile.open ("trace.example");
   
 Lexer *lexer = new Lexer(myfile);
 int count = 0;
 Token token = lexer->nextToken(); 
- while(token.tokenType != LexerType(END_OF_FILE)){
-	 	//std::cout  << token.toString()<< endl;
+
+//cout << lexer->isValueLetter('r') << endl;
+
+
+ while(token.tokenType != END_OF_FILE){
+	 	//std::cout  <<token.tokenType << " - " <<token.value<< endl;
 	 	token =lexer->nextToken(); 
 	 	count++;
  }
-if(count != 4207){
-	std::cout << "Error Lexer" << endl;
+if(count != 41){
+	std::cout << "Error Lexer" << count << endl;
 }
+
+ if(lexer->isDigit('0') != true){
+	 	std::cout << "Error Lexer isDigit" << endl;
+ }
+ 
+ 
   myfile.close();
 
+/* ********************************************************************* */ 
+ 
+ /* ********************************* TraceSequence ************************ */ 
+ 
+fstream file_TraceSequence;
+ file_TraceSequence.open ("trace.example");
+  TraceSequence *tracesequence = new TraceSequence();
+
+  tracesequence->populateFromStream(file_TraceSequence);
+   tracesequence->exportToString();
+  //cout << <<endl;
+  
+    file_TraceSequence.close();
+     delete tracesequence;
+     
+     
+    cout << "FINISH" << endl;
+    
+    
+
+/* ********************************************************************* */ 
 
 
 
