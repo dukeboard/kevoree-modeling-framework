afdf30fdf3af7f9c8152ed65b9094b04b862fcb5
JeD <jedartois@gmail.com>
2013-10-23 18:19:58 +0200

    update KMFContainer microframework c++

--- org.kevoree.modeling.cpp.microframework/src/main/cpp/org/kevoree/modeling/api/KMFContainer.h
+++ org.kevoree.modeling.cpp.microframework/src/main/cpp/org/kevoree/modeling/api/KMFContainer.h
@@ -2,52 +2,50 @@
 #define KMFContainer_H
 
 #include <string>
-using namespace std;
 #include "events/ModelElementListener.h"
 #include "trace/ModelTrace.h"
 #include <list>
+#include "utils/any.h"
+#include "utils/ModelVisitor.h"
+#include "utils/ModelAttributeVisitor.h"
+
+using std::string;
+using std::list;
 
 class KMFContainer 
 {
 
 public:
 
-    KMFContainer eContainer();
-    bool isReadOnly();
-    bool isRecursiveReadOnly();
-    void setInternalReadOnly();
-	void deleteContainer(); // can't use delete reserve c++ 
-	
-	
-	
-	bool modelEquals(KMFContainer similarObj);
-	
-    bool deepModelEquals(KMFContainer similarObj);
+    virtual KMFContainer eContainer(){}
+    virtual bool isReadOnly(){}
+    virtual bool isRecursiveReadOnly(){}
+    virtual void setInternalReadOnly(){}
+	virtual void deleteContainer(){} // can't use delete reserve c++ 
+	virtual bool modelEquals(KMFContainer similarObj){}
+    virtual bool deepModelEquals(KMFContainer similarObj){}
+    virtual string getRefInParent(){}
+    virtual KMFContainer findByPath(string query){}
+    virtual KMFContainer findByID(string relationName,string idP){}
+	virtual string path(){}
+    virtual string metaClassName(){}
+    virtual void reflexiveMutator(int mutatorType,string refName, void *value, bool setOpposite,bool fireEvent ){}
+    virtual list<any>  selectByQuery(string query){}
+    virtual void addModelElementListener(ModelElementListener lst){}
+    virtual void removeModelElementListener(ModelElementListener lst){}
+    virtual void removeAllModelElementListeners(){}
+    virtual void addModelTreeListener(ModelElementListener lst){}
+    virtual void removeModelTreeListener(ModelElementListener lst){}
+    virtual void removeAllModelTreeListeners(){}
     
-    
-    string getRefInParent();
-    KMFContainer findByPath(string query);
-    KMFContainer findByID(string relationName,string idP);
-	string path();
+    template <class A> // http://www.cplusplus.com/doc/tutorial/templates
+	A* findByPath(string query,A clazz);
+ 
+   virtual void visit(ModelVisitor visitor,bool recursive,bool containedReference ,bool nonContainedReference){}
+    virtual void visitAttributes(ModelAttributeVisitor visitor ){}
 
-    string metaClassName();
-    void reflexiveMutator(int mutatorType,string refName, void *value, bool setOpposite,bool fireEvent );
-   //  selectByQuery(query : String) : list<Any>
-   
-    void addModelElementListener(ModelElementListener lst);
-    void removeModelElementListener(ModelElementListener lst);
-    void removeAllModelElementListeners();
-    void addModelTreeListener(ModelElementListener lst);
-    void removeModelTreeListener(ModelElementListener lst);
-    void removeAllModelTreeListeners();
-    
-   // void findByPath<A>(query : String, clazz : Class<A>) : A?
-
-  //  void visit(visitor : org.kevoree.modeling.api.util.ModelVisitor, recursive : Boolean, containedReference : Boolean,nonContainedReference : Boolean)
-   // void visitAttributes(visitor : org.kevoree.modeling.api.util.ModelAttributeVisitor)
-
-    list<ModelTrace> createTraces(KMFContainer similarObj ,bool isInter ,bool isMerge ,bool onlyReferences,bool onlyAttributes );
-
+    virtual list<ModelTrace*> createTraces(KMFContainer similarObj ,bool isInter ,bool isMerge ,bool onlyReferences,bool onlyAttributes ) {}
+ModelVisitor t;
 };
 
 #endif
