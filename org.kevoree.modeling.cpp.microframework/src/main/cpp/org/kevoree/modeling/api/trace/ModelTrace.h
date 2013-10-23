0716e721cf986a6de132f72ce8f12cef527c5f0d
JeD <jedartois@gmail.com>
2013-10-23 15:13:19 +0200

    refactoring ModelTrace  microframework c++

--- org.kevoree.modeling.cpp.microframework/src/main/cpp/org/kevoree/modeling/api/trace/ModelTrace.h
+++ org.kevoree.modeling.cpp.microframework/src/main/cpp/org/kevoree/modeling/api/trace/ModelTrace.h
@@ -2,52 +2,47 @@
 #define MODEL_TRACE_H
 #include <string>
 #include <list>
+#include <iostream>
 #include "../utils/ActionType.h"
+
 
 using namespace std;
 
 class ModelTrace
 {
   public:	
-	virtual string toString () {};
+	virtual string toString ()
+	{
+	}
 };
-
 
 class ModelAddTrace : public ModelTrace
 {
 	
 public:
-  ModelAddTrace(string _srcPath, string _refName, string _previousPath,string _typeName)
-  {
-	 srcPath  =_srcPath;
-	 refName=_refName;
-	 previousPath =_previousPath;
-	 typeName=_typeName;
-  }
+   ModelAddTrace(string _srcPath, string _refName, string _previousPath,string _typeName);
+   ~ModelAddTrace();
+   virtual std::string toString ();
+    
   string srcPath;
   string refName;
   string previousPath;
   string typeName;
+
+};
+
+class ModelSetTrace:public ModelTrace
+{
+public: 
+  ModelSetTrace (string _srcPath, string _refName, string _objPath, string _content, string _typeName);
+  ~ModelSetTrace();
+  virtual std::string toString ();
   
-  virtual std::string toString ()
-  {
-    string buffer = "";
-
-    buffer.append ("{ \"traceType\" : " + ActionType (ADD) +
-		   " , \"src\" : \"" + srcPath + "\", \"refname\" : \"" +
-		   refName + "\"");
-    if (!previousPath.empty ())
-      {
-	buffer.append (", \"previouspath\" : \"" + previousPath + "\"");
-      }
-    if (typeName.empty ())
-      {
-	buffer.append (", \"typename\" : \"" + typeName + "\"");
-      }
-    buffer.append ("}");
-    return buffer;
-  }
-
+  string srcPath;
+  string refName;
+  string objPath;
+  string content;
+  string typeName;
 
 };
 
@@ -55,54 +50,12 @@ public:
 
 class ModelAddAllTrace:public ModelTrace
 {
-  ModelAddAllTrace (string _srcPath, string _refName,
-		    list < string > _previousPath, list < string > _typeName)
-  {
-    srcPath = _srcPath;
-    refName = _refName;
-    previousPath = _previousPath;
-    typeName = _typeName;
-  }
+public:
+  ModelAddAllTrace (string _srcPath, string _refName,list < string > &_previousPath, list < string > &_typeName);
+  ~ModelAddAllTrace();
+  virtual std::string toString ();
+  std::string mkString (list < string > ss);
 
-  std::string mkString (list < string > ss)
-  {
-    string buffer;
-    if (!ss.empty ())
-      {
-	return buffer;
-      }
-    std::list < std::string >::iterator it;
-    bool isFirst = true;
-    for (it = ss.begin (); it != ss.end (); ++it)
-      {
-	if (!isFirst)
-	  {
-	    buffer.append (",");
-	  }
-	buffer.append (*it);
-	isFirst = false;
-      }
-    return buffer;
-  }
-  std::string toString ()
-  {
-    string buffer;
-    buffer.append ("{ \"traceType\" : " + ActionType (ADD_ALL) +
-		   " , \"src\" : \"" + srcPath + "\", \"refname\" : \"" +
-		   refName + "\"");
-    if (!previousPath.empty ())
-      {
-	buffer.append (", \"previouspath\" : \"" + mkString (previousPath) +
-		       "\"");
-      }
-    if (typeName.empty ())
-      {
-	buffer.append (", \"typename\" : \"" + mkString (typeName) + "\"");
-      }
-    buffer.append ("}");
-    return buffer;
-  }
-  
   string srcPath;
   string refName;
   list < string > previousPath;
@@ -114,19 +67,11 @@ class ModelAddAllTrace:public ModelTrace
 
 class ModelRemoveTrace:public ModelTrace
 {
-  ModelRemoveTrace (string _srcPath, string _refName, string _objPath)
-  {
-    srcPath = _srcPath;
-    refName = _refName;
-    objPath = _objPath;
-  }
-  virtual std::string toString ()
-  {
-    return "{ \"traceType\" : " + ActionType (REMOVE) + " , \"src\" : \"" +
-      srcPath + "\", \"refname\" : \"" + refName + "\", \"objpath\" : \"" +
-      objPath + "\" }";
-  }
-private:
+public:
+  ModelRemoveTrace (string _srcPath, string _refName, string _objPath);
+  ~ModelRemoveTrace();
+  virtual std::string toString ();
+
   string srcPath;
   string refName;
   string objPath;
@@ -136,64 +81,17 @@ private:
 
 class ModelRemoveAllTrace:public ModelTrace
 {
-  ModelRemoveAllTrace (string _srcPath, string _refName)
-  {
-    srcPath = _srcPath;
-    refName = _refName;
-  }
-  virtual std::string toString ()
-  {
-    return "{ \"traceType\" : " + ActionType (REMOVE_ALL) +
-      " , \"src\" : \"" + srcPath + "\", \"refname\" : \"" + refName + "\" }";
-  }
-  
-  string srcPath;
-  string refName;
-};
-
-
-class ModelSetTrace:public ModelTrace
-{
 public:
+  ModelRemoveAllTrace (string _srcPath, string _refName);
+  ~ModelRemoveAllTrace();
+  virtual std::string toString ();
+
   string srcPath;
   string refName;
-  string objPath;
-  string content;
-  string typeName;
- 
-  ModelSetTrace (string _srcPath, string _refName, string _objPath, string _content, string _typeName)
-  {
-    srcPath = _srcPath;
-    refName = _refName;
-    typeName = _typeName;
-    content = _content;
-    typeName = _typeName;
-  }
-  virtual std::string toString ()
-  {
-    string buffer = "";
-    buffer.append ("{ \"traceType\" : " + ActionType (SET) +
-		   " , \"src\" : \"" + srcPath + "\", \"refname\" : \"" +
-		   refName + "\"");
-    if (!objPath.empty ())
-      {
-	buffer.append (", \"objpath\" : \"" + objPath + "\"");
-      }
-    if (!content.empty ())
-      {
-	buffer.append (", \"content\" : \"" + content + "\"");
-      }
-    if (!typeName.empty ())
-      {
-	buffer.append (", \"typename\" : \"" + typeName + "\"");
-      }
-    buffer.append ("}");
-
-    return buffer;
-
-  }
-
 };
+
+
+
 
 
 #endif
