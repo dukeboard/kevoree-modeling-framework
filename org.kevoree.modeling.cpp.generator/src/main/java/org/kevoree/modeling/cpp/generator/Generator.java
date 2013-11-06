d76189c5897a1cc4e76d5d5ff2410892eb0bb6cd
JeD <jedartois@gmail.com>
2013-11-06 18:26:28 +0100

    update generator

--- org.kevoree.modeling.cpp.generator/src/main/java/org/kevoree/modeling/cpp/generator/Generator.java
+++ org.kevoree.modeling.cpp.generator/src/main/java/org/kevoree/modeling/cpp/generator/Generator.java
@@ -12,6 +12,7 @@ import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.kevoree.modeling.cpp.generator.model.ClassGenerator;
 import org.kevoree.modeling.cpp.generator.model.FactoryGenerator;
+import org.kevoree.modeling.cpp.generator.utils.CheckerConstraint;
 import org.kevoree.modeling.cpp.generator.utils.ConverterDataTypes;
 import org.kevoree.modeling.cpp.generator.utils.FileManager;
 import org.kevoree.modeling.cpp.generator.utils.HelperGenerator;
@@ -46,38 +47,33 @@ public class Generator {
         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
         ResourceSetImpl rs = new ResourceSetImpl();
         XMIResource resource = (XMIResource) rs.createResource(fileUri);
-        resource.load(null) ;
-        EcoreUtil.resolveAll(resource);
+
         String package_name ="default";
         StringBuilder   list_file_h = new StringBuilder();
 
+        CheckerConstraint checkerConstraint = new CheckerConstraint(context);
+        checkerConstraint.verify(resource);
+
+
         ClassGenerator classGenerator =new ClassGenerator(context);
+
+
+
         FactoryGenerator factoryGenerator=null;
 
+        resource.load(null) ;
+        EcoreUtil.resolveAll(resource);
 
         for (Iterator i = resource.getAllContents(); i.hasNext();) {
             EObject eo = (EObject)i.next();
 
-            if(eo instanceof EClass){
+            if(eo instanceof EClass)
+            {
 
-                // checker
-                EClass c = (EClass) eo;
-                c.setName(ConverterDataTypes.getInstance().check_class(c.getName()));
-                for(EAttribute a : c.getEAllAttributes()){
-                    a.setName(ConverterDataTypes.getInstance().check_class(a.getName()));
-                }
-                for(EReference a : c.getEAllReferences()){
-                    a.setName(ConverterDataTypes.getInstance().check_class(a.getName()));
-
-                    a.getEReferenceType().setName(ConverterDataTypes.getInstance().check_class(a.getEReferenceType().getName()));
-                }
-
-                String curent = classGenerator.generateClass((EClass) eo);
-                FileManager.writeFile(context.getPackageGenerationDirectory()+"classes.cpp", curent,true);
-                classes.append(HelperGenerator.genIncludeLocal(((EClass) eo).getName()));
-
-
+                classGenerator.generateClass((EClass) eo);
                 factoryGenerator.generateFactory((EClass) eo);
+
+                classes.append(HelperGenerator.genIncludeLocal(((EClass) eo).getName()));
 
             }  else if(eo instanceof EPackage)
             {
