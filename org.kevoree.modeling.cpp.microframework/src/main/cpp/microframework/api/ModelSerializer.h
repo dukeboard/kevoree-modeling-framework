a31073f6a0cb483c1bbde99e982755fc30d63eec
JeD <jedartois@gmail.com>
2013-11-26 18:43:35 +0100

    define pure c++

--- org.kevoree.modeling.cpp.microframework/src/main/cpp/microframework/api/ModelSerializer.h
+++ org.kevoree.modeling.cpp.microframework/src/main/cpp/microframework/api/ModelSerializer.h
@@ -1,9 +1,10 @@
-#ifndef ModelSerializer_H
-#define ModelSerializer_H
+#ifndef __ModelSerializer_H
+#define __ModelSerializer_H
 
 #include <iostream>
 #include <string>
 #include <microframework/api/KMFContainer.h>
+#include <microframework/api/container/KMFContainerImpl.h>
 /**
  * Author: jedartois@gmail.com
  * Date: 24/10/13
@@ -13,8 +14,8 @@ class ModelSerializer
 {
 
 public:
-    void serializeToStream(KMFContainer *model, iostream  raw);
-    string serialize(KMFContainer *model);
+    virtual void serializeToStream(KMFContainer *model, iostream  raw)=0;
+    virtual string serialize(KMFContainer *model)=0;
 };
 
 
