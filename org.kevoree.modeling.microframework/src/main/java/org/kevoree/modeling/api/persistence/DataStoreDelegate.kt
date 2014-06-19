71f8a0617226df2af85b6b44e6fa089a46d7e14f
Francois Fouquet <fouquet.f@gmail.com>
2014-06-19 18:34:03 +0200

    fix KMF delegate

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/persistence/DataStoreDelegate.kt
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/persistence/DataStoreDelegate.kt
@@ -1,29 +1,41 @@
 package org.kevoree.modeling.api.persistence
 
+import java.util.HashSet
+
 /**
  * Created by duke on 6/19/14.
  */
 
-class DataStoreDelegate(val proxy: DataStore, val real: DataStore) : DataStore {
+class DataStoreDelegate(val parent: DataStore, val local: DataStore) : DataStore {
 
     override fun get(segment: String, key: String): String? {
-        throw UnsupportedOperationException()
+        var result = local.get(segment,key)
+        if(result == null){
+            result = parent.get(segment,key)
+        }
+        return result
     }
     override fun remove(segment: String, key: String) {
-        throw UnsupportedOperationException()
+        local.remove(segment,key)
     }
     override fun sync() {
-        real.sync()
+        local.sync()
     }
     override fun getSegments(): Set<String> {
-        throw UnsupportedOperationException()
+        val concat = HashSet<String>()
+        concat.addAll(local.getSegments())
+        concat.addAll(parent.getSegments())
+        return concat
     }
     override fun getSegmentKeys(segment: String): Set<String> {
-        throw UnsupportedOperationException()
+        val concat = HashSet<String>()
+        concat.addAll(parent.getSegmentKeys(segment))
+        concat.addAll(local.getSegmentKeys(segment))
+        return concat
     }
 
     override fun put(segment: String, key: String, value: String) {
-        real.put(segment, key, value)
+        local.put(segment, key, value)
     }
 
 
