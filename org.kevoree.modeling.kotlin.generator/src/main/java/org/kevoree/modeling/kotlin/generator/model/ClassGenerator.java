fd292099750f631e0ee296665076c43aa4907307
Grégory NAIN <gregory.nain@gmail.com>
2014-07-25 19:18:54 +0200

    replace error by print

--- org.kevoree.modeling.kotlin.generator/src/main/java/org/kevoree/modeling/kotlin/generator/model/ClassGenerator.java
+++ org.kevoree.modeling.kotlin.generator/src/main/java/org/kevoree/modeling/kotlin/generator/model/ClassGenerator.java
@@ -673,7 +673,7 @@ public class ClassGenerator {
 
 
         res.append("} else {\n");
-        res.append("error(\"Element not added, because a "+typeRefName+" already exists with id:\"+_key_)\n");
+        res.append("println(\""+cls.getName()+"::add"+ProcessorHelper.getInstance().toCamelCase(ref)+" -> Element not added, because a "+typeRefName+" already exists with id:\"+_key_)\n");
         res.append("}\n");
         res.append("}\n");
         return res.toString();
