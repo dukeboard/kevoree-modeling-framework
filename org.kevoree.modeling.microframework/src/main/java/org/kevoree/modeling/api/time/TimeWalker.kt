98343722a6835cdb29380ce3c9d177868e536749
Grégory NAIN <gregory.nain@gmail.com>
2014-07-30 14:42:36 +0200

    Adding a time walker

---
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/time/TimeWalker.kt
@@ -0,0 +1,8 @@
+package org.kevoree.modeling.api.time
+
+/**
+ * Created by gregory.nain on 30/07/2014.
+ */
+public trait TimeWalker {
+    fun walk(timePoint : TimePoint);
+}
--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/time/blob/TimeMeta.kt
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/time/blob/TimeMeta.kt
@@ -1,5 +1,7 @@
 package org.kevoree.modeling.api.time.blob
 
+import org.kevoree.modeling.api.time.TimeWalker
+
 /**
  * Created by duke on 6/4/14.
  */
@@ -21,5 +23,41 @@ class TimeMeta() {
     fun load(payload: String) {
         versionTree.unserialize(payload)
     }
+
+    fun walk(walker : TimeWalker) {
+        walk(walker, false)
+    }
+
+    fun walk(walker : TimeWalker, descending : Boolean = false) {
+        if(versionTree.root == null){
+            return
+        }
+        if(descending) {
+            internalWalkerDec(versionTree.root!!, walker);
+        } else {
+            internalWalkerAsc(versionTree.root!!, walker);
+        }
+    }
+
+    private fun internalWalkerAsc(currentNode : Node, walker : TimeWalker) {
+        if (currentNode.left != null) {
+            internalWalkerAsc(currentNode.left!!, walker)
+        }
+        walker.walk(currentNode.key)
+        if (currentNode.right != null) {
+            internalWalkerAsc(currentNode.right!!,walker)
+        }
+    }
+
+    private fun internalWalkerDec(currentNode : Node, walker : TimeWalker) {
+        if (currentNode.right != null) {
+            internalWalkerDec(currentNode.right!!,walker)
+        }
+        walker.walk(currentNode.key)
+        if (currentNode.left != null) {
+            internalWalkerDec(currentNode.left!!, walker)
+        }
+    }
+
 
 }
