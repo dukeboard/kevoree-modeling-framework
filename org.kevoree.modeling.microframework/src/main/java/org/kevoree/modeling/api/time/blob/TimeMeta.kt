26b32830abe13fc5c746b89264fd50b98faa39c9
Grégory NAIN <gregory.nain@gmail.com>
2014-07-31 14:51:38 +0200

    Update Walkers

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/time/blob/TimeMeta.kt
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/time/blob/TimeMeta.kt
@@ -1,6 +1,7 @@
 package org.kevoree.modeling.api.time.blob
 
 import org.kevoree.modeling.api.time.TimeWalker
+import org.kevoree.modeling.api.time.TimePoint
 
 /**
  * Created by duke on 6/4/14.
@@ -25,40 +26,70 @@ class TimeMeta() {
         dirty = false;
     }
 
-    fun walk(walker : TimeWalker) {
-        walk(walker, false)
+
+    fun walkAsc(walker : TimeWalker) {
+        walk(walker : TimeWalker, null, null, false)
     }
 
-    fun walk(walker : TimeWalker, descending : Boolean = false) {
-        if(versionTree.root == null){
-            return
-        }
-        if(descending) {
-            internalWalkerDec(versionTree.root!!, walker);
-        } else {
-            internalWalkerAsc(versionTree.root!!, walker);
-        }
+    fun walkDesc(walker : TimeWalker) {
+        walk(walker : TimeWalker, null, null, false)
     }
 
-    private fun internalWalkerAsc(currentNode : Node, walker : TimeWalker) {
+    fun walkRangeAsc(walker : TimeWalker, from : TimePoint?, to : TimePoint?) {
+        walk(walker : TimeWalker, from, to)
+    }
+
+    fun walkRangeDesc(walker : TimeWalker, from : TimePoint?, to : TimePoint?) {
+        walk(walker : TimeWalker, from, to, false)
+    }
+
+    private fun internalWalkerAsc(currentNode : Node, walker : TimeWalker, limit : TimePoint? = null ) {
         if (currentNode.left != null) {
             internalWalkerAsc(currentNode.left!!, walker)
         }
         walker.walk(currentNode.key)
-        if (currentNode.right != null) {
-            internalWalkerAsc(currentNode.right!!,walker)
+        if(limit == null || currentNode.key.compareTo(limit) < 0) {
+            if (currentNode.right != null) {
+                internalWalkerAsc(currentNode.right!!, walker)
+            }
         }
     }
 
-    private fun internalWalkerDec(currentNode : Node, walker : TimeWalker) {
+    private fun internalWalkerDesc(currentNode : Node, walker : TimeWalker, limit : TimePoint? = null ) {
         if (currentNode.right != null) {
-            internalWalkerDec(currentNode.right!!,walker)
+            internalWalkerDesc(currentNode.right!!,walker)
         }
         walker.walk(currentNode.key)
-        if (currentNode.left != null) {
-            internalWalkerDec(currentNode.left!!, walker)
+        if(limit == null || currentNode.key.compareTo(limit) > 0) {
+            if (currentNode.left != null) {
+                internalWalkerDesc(currentNode.left!!, walker)
+            }
         }
     }
+
+    private fun walk(walker : TimeWalker, from : TimePoint? = null, to : TimePoint? = null, ascending : Boolean = true) {
+        if(versionTree.root == null){
+            return
+        }
+
+        var startNode : Node? = null
+        if(from != null) {
+            startNode = versionTree.lowerOrEqual(from)
+            if(startNode == null) {
+                startNode = versionTree.upper(from)
+            }
+            if(startNode == null) {
+                return;
+            }
+        }
+
+        if(ascending) {
+            internalWalkerAsc(if(startNode==null){versionTree.root!!}else{startNode!!}, walker, to);
+        } else {
+            internalWalkerDesc(if(startNode==null){versionTree.root!!}else{startNode!!}, walker, to);
+        }
+    }
+
 
 
 }
