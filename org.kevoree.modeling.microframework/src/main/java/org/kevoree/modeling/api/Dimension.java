09f63320ac1f8ae29a44787bce3be01fd3d4da98
Francois Fouquet <fouquet.f@gmail.com>
2014-10-02 17:22:40 +0200

    dimension

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/Dimension.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/Dimension.java
@@ -1,23 +1,34 @@
 package org.kevoree.modeling.api;
 
-import org.kevoree.modeling.api.time.TimeView;
 import org.kevoree.modeling.api.time.TimeTree;
+import org.kevoree.modeling.api.time.TimeView;
+
+import java.util.Set;
 
 /**
  * Created by duke on 9/30/14.
  */
 
 public interface Dimension {
-    fun key(): String
-    fun parent(callback: Callback<Dimension?>)
-    fun children(callback: Callback<Set<Dimension>>)
-    fun fork(callback: Callback<Dimension?>)
-    fun save(callback: Callback<Boolean>)
-    fun delete(callback: Callback<Boolean>)
-    fun unload(callback: Callback<Boolean>)
 
-    fun time(timePoint: Long): TimeView
-    fun globalTimeTree(): TimeTree
-    fun timeTree(path: String): TimeTree
+    public String key();
+
+    public void parent(Callback<Dimension> callback);
+
+    public void children(Callback<Set<Dimension>> callback);
+
+    public void fork(Callback<Dimension> callback);
+
+    public void save(Callback<Boolean> callback);
+
+    public void delete(Callback<Boolean> callback);
+
+    public void unload(Callback<Boolean> callback);
+
+    public TimeView time(Long timePoint);
+
+    public TimeTree globalTimeTree();
+
+    public TimeTree timeTree(String path);
 
 }
