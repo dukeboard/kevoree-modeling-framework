71f4b43b72a6ca8f2efa8d843ca6b549da93253a
Francois Fouquet <fouquet.f@gmail.com>
2014-10-17 16:34:25 +0200

    cleanup serializer

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/json/JSONModelSerializer.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/json/JSONModelSerializer.java
@@ -6,6 +6,7 @@ import org.kevoree.modeling.api.ModelSerializer;
 import org.kevoree.modeling.api.ModelVisitor;
 
 import java.io.*;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -40,16 +41,75 @@ public class JSONModelSerializer implements ModelSerializer {
     public void serializeToStream(KObject model, OutputStream raw, Callback<Throwable> error) {
         final PrintStream out = new PrintStream(new BufferedOutputStream(raw), false);
         out.print("[\n");
-        out.print(model.toJSON());
+        printJSON(model, out);
         model.graphVisit((elem, visitor) -> {
             out.print(",");
-            out.print(elem.toJSON());
+            printJSON(elem, out);
             visitor.on(ModelVisitor.Result.CONTINUE);
         }, (t) -> {
             out.print("]\n");
             out.flush();
             error.on(null);
         });
+    }
+
+
+    public void printJSON(KObject elem, PrintStream builder) {
+        builder.append("{\n");
+        builder.append("\t\"@meta\" : \"");
+        builder.append(elem.metaClass().metaName());
+        builder.append("\",\n");
+        builder.append("\t\"@kid\" : \"");
+        builder.append(elem.kid() + "");
+        builder.append("\",\n");
+        for (int i = 0; i < elem.metaAttributes().length; i++) {
+            Object payload = elem.get(elem.metaAttributes()[i]);
+            if (payload != null) {
+                builder.append("\t");
+                builder.append("\"");
+                builder.append(elem.metaAttributes()[i].metaName());
+                builder.append("\":\"");
+                builder.append(payload.toString());
+                builder.append("\",\n");
+            }
+        }
+        for (int i = 0; i < elem.metaReferences().length; i++) {
+            Object[] raw = elem.factory().dimension().universe().storage().raw(elem.dimension(), elem.now(), elem.kid());
+            Object payload = null;
+            if (raw != null) {
+                payload = raw[elem.metaReferences()[i].index()];
+            }
+            if (payload != null) {
+                builder.append("\t");
+                builder.append("\"");
+                builder.append(elem.metaReferences()[i].metaName());
+                builder.append("\":");
+                if (elem.metaReferences()[i].single()) {
+                    builder.append("\"");
+                    builder.append(payload.toString());
+                    builder.append("\"");
+                } else {
+                    Set<Long> elems = (Set<Long>) payload;
+                    Long[] elemsArr = elems.toArray(new Long[elems.size()]);
+                    boolean isFirst = true;
+                    builder.append(" [");
+                    for (int j = 0; j < elemsArr.length; j++) {
+                        if (!isFirst) {
+                            builder.append(",");
+                        }
+                        builder.append("\"");
+                        builder.append(elemsArr[j] + "");
+                        builder.append("\"");
+                        isFirst = false;
+                    }
+                    builder.append("]");
+                }
+                builder.append(",\n");
+            }
+        }
+        // int lastcomma = builder.lastIndexOf(",");
+        //  builder.setCharAt(lastcomma, ' ');
+        builder.append("}\n");
     }
 
 }
