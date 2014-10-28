781a6a019fe742c1814d43278d8530ab25579e37
Francois Fouquet <fouquet.f@gmail.com>
2014-10-28 13:49:23 +0100

    add polynomialTest

---
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/microframework/test/polynomial/PolynomialTest.java
@@ -0,0 +1,18 @@
+package org.kevoree.modeling.microframework.test.polynomial;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by duke on 10/28/14.
+ */
+public class PolynomialTest {
+
+    @Test
+    public void test(){
+        //TODO
+        assertTrue(!"GoAssaad".isEmpty());
+
+    }
+
+}
