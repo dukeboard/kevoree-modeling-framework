b2d123427673acd8a9ebe88ba017dd7cb1af3892
Francois Fouquet <fouquet.f@gmail.com>
2014-10-29 11:19:38 +0100

    first version of polynomial into KMF :-)

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/strategy/PolynomialExtrapolationStrategy.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/strategy/PolynomialExtrapolationStrategy.java
@@ -1,7 +1,11 @@
 package org.kevoree.modeling.api.strategy;
 
 import org.kevoree.modeling.api.KObject;
+import org.kevoree.modeling.api.data.KStore;
 import org.kevoree.modeling.api.meta.MetaAttribute;
+import org.kevoree.modeling.api.polynomial.DefaultPolynomialExtrapolation;
+import org.kevoree.modeling.api.polynomial.PolynomialExtrapolation;
+import org.kevoree.modeling.api.polynomial.util.Prioritization;
 
 /**
  * Created by duke on 10/28/14.
@@ -17,12 +21,24 @@ public class PolynomialExtrapolationStrategy implements ExtrapolationStrategy {
 
     @Override
     public Object extrapolate(KObject current, MetaAttribute attribute, KObject[] dependencies) {
-
-        return null;
+        Object[] internalPayload = current.view().dimension().universe().storage().raw(current, KStore.AccessMode.READ);
+        PolynomialExtrapolation pol = (PolynomialExtrapolation) internalPayload[attribute.index()];
+        if (pol != null) {
+            return pol.extrapolate(current.now());
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void mutate(KObject current, MetaAttribute attribute, Object payload, KObject[] dependencies) {
-
+        Object[] internalPayload = current.view().dimension().universe().storage().raw(current, KStore.AccessMode.READ);
+        PolynomialExtrapolation pol = (PolynomialExtrapolation) internalPayload[attribute.index()];
+        if (pol == null || !pol.insert(current.now(), Double.parseDouble(payload.toString()))) {
+            pol = new DefaultPolynomialExtrapolation(current.now(), 0.1 /* TODO call attrbute definition */, 20, 1, Prioritization.LOWDEGREES);
+            pol.insert(current.now(), Double.parseDouble(payload.toString()));
+            Object[] internalPayloadWrite = current.view().dimension().universe().storage().raw(current, KStore.AccessMode.WRITE);
+            internalPayloadWrite[attribute.index()] = pol;
+        }
     }
 }
