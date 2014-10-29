f4e79a3652f1bf1d81fefb6ce9fbdd1e7fb61365
Francois Fouquet <fouquet.f@gmail.com>
2014-10-29 08:25:41 +0100

    implement cloner :-)

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/clone/DefaultModelCloner.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/clone/DefaultModelCloner.java
@@ -1,11 +1,9 @@
 package org.kevoree.modeling.api.clone;
 
-import org.kevoree.modeling.api.Callback;
-import org.kevoree.modeling.api.KView;
-import org.kevoree.modeling.api.KObject;
-import org.kevoree.modeling.api.ModelCloner;
+import org.kevoree.modeling.api.*;
 
-public class DefaultModelCloner implements ModelCloner {
+public class DefaultModelCloner implements ModelCloner<KObject> {
+
     private KView factory;
 
     public DefaultModelCloner(KView factory) {
@@ -13,97 +11,18 @@ public class DefaultModelCloner implements ModelCloner {
     }
 
     @Override
-    public void clone(KObject o, Callback callback) {
-        throw new UnsupportedOperationException();
-    }
-
-    /*
-    private fun createContext(): MutableMap<KObject<*>, KObject<*>> {
-        return IdentityHashMap<KObject<*>, KObject<*>>()
-    }
-
-    fun clone<A : KObject>(o: A): A? {
-        return clone(o, false)
-    }
-
-    fun clone<A : KObject>(o: A, readOnly: Boolean): A? {
-        return clone(o, readOnly, false)
-    }
-
-    private fun cloneModelElem(src: KObject<*>): KObject {
-        val clonedSrc = view.create(src.metaClassName())
-        val attributesCloner = object : ModelAttributeVisitor {
-            public override fun visit(value: Any?, name: STRING, parent: KObject) {
-                if (value != null) {
-                    if (value is ArrayList<*>) {
-                        val clonedList = ArrayList<Any>()
-                        clonedList.addAll(value as Collection<Any>);
-                        clonedSrc.reflexiveMutator(ActionType.SET, name, clonedList, false, false)
-                    } else {
-                        clonedSrc.reflexiveMutator(ActionType.SET, name, value, false, false)
+    public void clone(KObject originalObject, Callback<KObject> callback) {
+        originalObject.view().dimension().fork(new Callback<KDimension>() {
+            @Override
+            public void on(KDimension o) {
+                o.time(originalObject.view().now()).lookup(originalObject.uuid(), new Callback<KObject>() {
+                    @Override
+                    public void on(KObject clonedObject) {
+                        callback.on(clonedObject);
                     }
-                }
+                });
             }
-        }
-        src.visitAttributes(attributesCloner);
-        return clonedSrc;
+        });
     }
-
-    private fun resolveModelElem(src: KObject, target: KObject, context: Map<KObject, KObject>, mutableOnly: Boolean) {
-        val refResolver = object : ModelVisitor() {
-            public override fun visit(elem: KObject, refNameInParent: STRING, parent: KObject) {
-                if (mutableOnly && elem.isRecursiveReadOnly()) {
-                    target.reflexiveMutator(ActionType.ADD, refNameInParent, elem, false, false)
-                } else {
-                    val elemResolved = context.get(elem)
-                    if (elemResolved == null) {
-                        throw Exception("Cloner error, not self-contain model, the element " + elem.path() + " is contained in the root element")
-                    }
-                    target.reflexiveMutator(ActionType.ADD, refNameInParent, elemResolved, false, false)
-                }
-            }
-        }
-        src.visit(refResolver, false, true, true);
-    }
-
-    private fun clone<A : KObject>(o: A, readOnly: Boolean, mutableOnly: Boolean): A? {
-        val context = createContext()
-        val clonedObject = cloneModelElem(o);
-        context.put(o, clonedObject)
-        val cloneGraphVisitor = object : ModelVisitor() {
-            override public fun visit(elem: KObject, refNameInParent: STRING, parent: KObject) {
-                if (mutableOnly && elem.isRecursiveReadOnly()) {
-                    noChildrenVisit();
-                } else {
-                    context.put(elem, cloneModelElem(elem))
-                }
-            }
-        }
-        //clone the entire object graph (i.e. object+attributes)
-        o.visit(cloneGraphVisitor, true, true, false)
-        val resolveGraphVisitor = object : ModelVisitor() {
-            override public fun visit(elem: KObject, refNameInParent: STRING, parent: KObject<*>) {
-                if (mutableOnly && elem.isRecursiveReadOnly()) {
-                    //noChildrenVisit(); TODO check this behavior on partial clone
-                } else {
-                    val clonedObj = context.get(elem)!!
-                    resolveModelElem(elem, clonedObj, context, mutableOnly)
-                }
-            }
-        }
-        //resolve root references first
-        resolveModelElem(o, clonedObject, context, mutableOnly)
-        //copy graph references
-        o.visit(resolveGraphVisitor, true, true, false)
-        if (readOnly) {
-            clonedObject.setInternalReadOnly()
-        }
-        if (o.isRoot()) {
-            view.root(clonedObject)
-        }
-        return clonedObject as A
-    }
-    */
-
 
 }
