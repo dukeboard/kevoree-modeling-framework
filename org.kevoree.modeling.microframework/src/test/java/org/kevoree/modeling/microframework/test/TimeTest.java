057af46c976232f0bdd7f3262614e2c75417df41
Thomas Hartmann <thomi.hartmann@gmail.com>
2014-10-31 14:59:04 +0100

    extended test for kmf time

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/microframework/test/TimeTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/microframework/test/TimeTest.java
@@ -6,8 +6,7 @@ import org.kevoree.modeling.api.KObject;
 import org.kevoree.modeling.api.data.MemoryKDataBase;
 import org.kevoree.modeling.microframework.test.cloud.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * Created by thomas on 10/21/14.
@@ -15,7 +14,7 @@ import static org.junit.Assert.assertNotNull;
 public class TimeTest {
 
     @Test
-    public void timeTest() {
+    public void timeCreationTest() {
         CloudUniverse universe = new CloudUniverse(new MemoryKDataBase());
         universe.newDimension(new Callback<CloudDimension>() {
             @Override
@@ -27,76 +26,195 @@ public class TimeTest {
                 assertNotNull("Time0 should be created", t0);
                 assertEquals("Time0 should be created with time 0", t0.now(), 0l);
 
-                final Node node0 = t0.createNode();
-                assertEquals("Node0 should be created with time 0", node0.now(), 0l);
+                // create time1
+                final CloudView t1 = dimension0.time(1l);
+                assertNotNull("Time1 should be created", t1);
+                assertEquals("Time1 should be created with time 0", t1.now(), 1l);
+            }
+        });
+    }
 
+    @Test
+    public void simpleTimeNavigationTest() {
+        CloudUniverse universe = new CloudUniverse(new MemoryKDataBase());
+        universe.newDimension(new Callback<CloudDimension>() {
+            @Override
+            public void on(CloudDimension dimension0) {
+                assertNotNull("Dimension should be created", dimension0);
+
+                // create time0
+                final CloudView t0 = dimension0.time(0l);
+
+                // create node0 and element0 and link them
+                final Node node0 = t0.createNode();
                 Element element0 = t0.createElement();
                 node0.setElement(element0);
-                assertEquals("Node0 should be created with time 0", element0.now(), 0l);
 
-                // create time 0
-                final CloudView t1 = dimension0.time(1l);
-
-                // create element at time t1
-                final Element element1 = t1.createElement();
-                assertEquals(element1.now(), t1.now());
-
-                t1.lookup(node0.uuid(), new Callback<KObject>() {
+                node0.getElement(new Callback<Element>() {
                     @Override
-                    public void on(KObject kObject) {
-                        ((Node) kObject).setElement(element1);
-
-                        // lookup node
-                        t0.lookup(node0.uuid(), new Callback<KObject>() {
-                            @Override
-                            public void on(KObject kObject_t0) {
-                                assertEquals("Node should be resolved with time 0", kObject_t0.now(), 0l);
-                            }
-                        });
-
-                        //
-                        t1.lookup(node0.uuid(), new Callback<KObject>() {
-                            @Override
-                            public void on(KObject kObject_t1) {
-                                assertEquals("Node should be resolved with time 1", kObject_t1.now(), 1l);
-                            }
-                        });
-
-                        ((Node) kObject).getElement(new Callback<Element>() {
-                            @Override
-                            public void on(Element e) {
-                                assertNotNull(e);
-                            }
-                        });
-
+                    public void on(Element element) {
+                        assertEquals(element, element0);
+                        assertEquals(element.now(), t0.now());
                     }
                 });
 
-                // test navigation from node to element at t1
+                t0.lookup(node0.uuid(), new Callback<KObject>() {
+                    @Override
+                    public void on(KObject kObject) {
+                        ((Node) kObject).getElement(new Callback<Element>() {
+                            @Override
+                            public void on(Element element) {
+                                assertEquals(element, element0);
+                                assertEquals(element.now(), t0.now());
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+
+    @Test
+    public void distortedTimeNavigationTest() {
+        CloudUniverse universe = new CloudUniverse(new MemoryKDataBase());
+        universe.newDimension(new Callback<CloudDimension>() {
+            @Override
+            public void on(CloudDimension dimension0) {
+                assertNotNull("Dimension should be created", dimension0);
+
+                // create time0
+                final CloudView t0 = dimension0.time(0l);
+                // create node0
+                final Node node0 = t0.createNode();
+
+                node0.getElement(new Callback<Element>() {
+                    @Override
+                    public void on(Element element) {
+                        assertNull(element);
+                    }
+                });
+
+                t0.lookup(node0.uuid(), new Callback<KObject>() {
+                    @Override
+                    public void on(KObject kObject) {
+                        ((Node) kObject).getElement(new Callback<Element>() {
+                            @Override
+                            public void on(Element element) {
+                                assertNull(element);
+                            }
+                        });
+                    }
+                });
+
+                // create time1
+                final CloudView t1 = dimension0.time(1l);
+
+                // create elem1 and link node0 to elem1
+                final Element elem1 = t1.createElement();
+                node0.setElement(elem1);
+
+                // at t0 node0.getElement should be null
+                t0.lookup(node0.uuid(), new Callback<KObject>() {
+                    @Override
+                    public void on(KObject kObject) {
+                        ((Node) kObject).getElement(new Callback<Element>() {
+                            @Override
+                            public void on(Element element) {
+                                assertNull(element);
+                            }
+                        });
+                    }
+                });
+
+                // at t1 node0.getElement should return elem1
                 t1.lookup(node0.uuid(), new Callback<KObject>() {
                     @Override
                     public void on(KObject kObject) {
                         ((Node) kObject).getElement(new Callback<Element>() {
                             @Override
                             public void on(Element element) {
-                                assertNotNull("Element1 should be resolved", element);
-                                assertEquals("Element1 should have time 1", element.now(), 1l);
+                                assertNotNull(element);
+                                assertEquals(element, elem1);
+                                assertEquals(element.now(), t1.now());
                             }
                         });
                     }
                 });
 
-
-                CloudView t2 = dimension0.time(2l);
-                Element element2 = t2.createElement();
-
-
-                // protected against null callback?
-                t0.lookup(node0.uuid(), null);
-                
             }
         });
-
-
     }
+
+    @Test
+    public void objectModificationTest() {
+        CloudUniverse universe = new CloudUniverse(new MemoryKDataBase());
+        universe.newDimension(new Callback<CloudDimension>() {
+            @Override
+            public void on(CloudDimension dimension0) {
+                assertNotNull("Dimension should be created", dimension0);
+
+                // create time0
+                final CloudView t0 = dimension0.time(0l);
+                // create node0
+                final Node node0 = t0.createNode();
+
+                node0.getElement(new Callback<Element>() {
+                    @Override
+                    public void on(Element element) {
+                        assertNull(element);
+                    }
+                });
+
+                t0.lookup(node0.uuid(), new Callback<KObject>() {
+                    @Override
+                    public void on(KObject kObject) {
+                        ((Node) kObject).getElement(new Callback<Element>() {
+                            @Override
+                            public void on(Element element) {
+                                assertNull(element);
+                            }
+                        });
+                    }
+                });
+
+                // create time1
+                final CloudView t1 = dimension0.time(1l);
+
+                // create elem1 and link node0 to elem1
+                final Element elem1 = t1.createElement();
+                node0.setElement(elem1);
+
+                // at t0 node0.getElement should be null
+                t0.lookup(node0.uuid(), new Callback<KObject>() {
+                    @Override
+                    public void on(KObject kObject) {
+                        ((Node) kObject).getElement(new Callback<Element>() {
+                            @Override
+                            public void on(Element element) {
+                                assertNull(element);
+                            }
+                        });
+                    }
+                });
+
+                // at t1 node0.getElement should return elem1
+                t1.lookup(node0.uuid(), new Callback<KObject>() {
+                    @Override
+                    public void on(KObject kObject) {
+                        ((Node) kObject).getElement(new Callback<Element>() {
+                            @Override
+                            public void on(Element element) {
+                                assertNotNull(element);
+                                assertEquals(element, elem1);
+                                assertEquals(element.now(), t1.now());
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
+    }
+
 }
