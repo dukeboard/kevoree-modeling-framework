df9adb19788952d9ce3e3b5bcfc0c5400c1ad684
Francois Fouquet <fouquet.f@gmail.com>
2014-10-31 09:54:01 +0100

    fix

---
+++ org.kevoree.modeling.idea/gen/org/kevoree/modeling/idea/psi/MetaModelEnumDeclaration.java
@@ -0,0 +1,16 @@
+// This is a generated file. Not intended for manual editing.
+package org.kevoree.modeling.idea.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface MetaModelEnumDeclaration extends PsiElement {
+
+  @NotNull
+  List<MetaModelEnumElemDeclaration> getEnumElemDeclarationList();
+
+  @Nullable
+  MetaModelTypeDeclaration getTypeDeclaration();
+
+}
