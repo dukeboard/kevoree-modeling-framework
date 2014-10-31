9e89f08a6ac51b67fea25175e721bbdc854ec6a5
Grégory NAIN <gregory.nain@gmail.com>
2014-10-31 20:04:25 +0100

    Generics parameters

--- org.kevoree.modeling.java2typescript/src/main/java/com/siliconmint/ts/translator/MethodTranslator.java
+++ org.kevoree.modeling.java2typescript/src/main/java/com/siliconmint/ts/translator/MethodTranslator.java
@@ -9,90 +9,122 @@ import java.util.List;
 
 public class MethodTranslator extends Translator<PsiMethod> {
 
-  private static final Joiner joiner = Joiner.on(", ");
+    private static final Joiner joiner = Joiner.on(", ");
 
-  @Override
-  public void translate(PsiElementVisitor visitor, PsiMethod element, TranslationContext ctx) {
+    @Override
+    public void translate(PsiElementVisitor visitor, PsiMethod element, TranslationContext ctx) {
 
-    if (!ctx.hasWhitespace()) {
-      ctx.append("\n");
-    }
+        if (!ctx.hasWhitespace()) {
+            ctx.append("\n");
+        }
 
-    PsiModifierList modifierList = element.getModifierList();
+        PsiModifierList modifierList = element.getModifierList();
 
-    if (element.isConstructor()) {
-      ctx.print("constructor");
-    } else {
-      if (element.getContainingClass().isInterface()) {
-        ctx.print("");
-      } else {
-        if (modifierList.hasModifierProperty("private")) {
-          ctx.print("private ");
+        if (element.isConstructor()) {
+            ctx.print("constructor");
         } else {
-          ctx.print("public ");
+            if (element.getContainingClass().isInterface()) {
+                ctx.print("");
+            } else {
+                if (modifierList.hasModifierProperty("private")) {
+                    ctx.print("private ");
+                } else {
+                    ctx.print("public ");
+                }
+            }
+
+            if (modifierList.hasModifierProperty("static")) {
+                ctx.append("static ");
+            }
+            ctx.append(element.getName());
+
+            PsiTypeParameter[] typeParameters = element.getTypeParameters();
+            if (typeParameters != null && typeParameters.length > 0){
+                ctx.append('<');
+                for (int i=0; i < typeParameters.length; i++){
+                    ctx.append(typeParameters[i].getName());
+                    if (i != typeParameters.length - 1) {
+                        ctx.append(", ");
+                    }
+                }
+                ctx.append("> ");
+            }
+
         }
-      }
 
-      if (modifierList.hasModifierProperty("static")) {
-        ctx.append("static ");
-      }
-      ctx.append(element.getName());
+        ctx.append('(');
+        List<String> params = new ArrayList<String>();
+        StringBuilder paramSB = new StringBuilder();
 
-      PsiTypeParameter[] typeParameters = element.getTypeParameters();
-      if (typeParameters != null && typeParameters.length > 0){
-        ctx.append('<');
-        for (int i=0; i < typeParameters.length; i++){
-          ctx.append(typeParameters[i].getName());
-          if (i != typeParameters.length - 1) {
-            ctx.append(", ");
-          }
+        for (PsiParameter parameter : element.getParameterList().getParameters()) {
+            paramSB.setLength(0);
+            if (parameter.isVarArgs()) {
+                paramSB.append("...");
+            }
+            paramSB.append(parameter.getName());
+            paramSB.append(": ");
+            //paramSB.append(TypeHelper.getParameterType(parameter));
+
+            String resolvedType = TypeHelper.getParameterType(parameter);
+            Integer generics = ctx.getGenerics().get(resolvedType);
+            paramSB.append(resolvedType);
+            if(generics != null) {
+                paramSB.append("<");
+                for(int i = 0; i < generics ; i++) {
+                    paramSB.append("any");
+                    if(i < generics-1) {
+                        paramSB.append(",");
+                    }
+                }
+                paramSB.append(">");
+            }
+
+
+            params.add(paramSB.toString());
         }
-        ctx.append("> ");
-      }
+        ctx.append(joiner.join(params));
+        ctx.append(')');
 
+        if (!element.isConstructor()) {
+            ctx.append(": ");
+
+            //String returnType = TypeHelper.getMethodReturnType(element);
+            //ctx.append(returnType);
+
+            String resolvedType = TypeHelper.getMethodReturnType(element);
+            Integer generics = ctx.getGenerics().get(resolvedType);
+            ctx.append(resolvedType);
+            if(generics != null) {
+                ctx.append("<");
+                for(int i = 0; i < generics ; i++) {
+                    ctx.append("any");
+                    if(i < generics-1) {
+                        ctx.append(",");
+                    }
+                }
+                ctx.append(">");
+            }
+
+
+        }
+
+        PsiClass containingClass = (PsiClass) element.getParent();
+        if (!containingClass.isInterface()){
+            ctx.append(" {\n");
+
+            ctx.increaseIdent();
+            if (modifierList.hasModifierProperty("abstract") || element.getBody() == null) {
+                ctx.print("throw \"Abstract method\";\n");
+            } else {
+                element.getBody().accept(visitor);
+            }
+            ctx.decreaseIdent();
+
+            ctx.print("}\n");
+        } else {
+            ctx.append(";\n");
+        }
+        ctx.append("\n");
     }
-
-    ctx.append('(');
-    List<String> params = new ArrayList<String>();
-    StringBuilder paramSB = new StringBuilder();
-
-    for (PsiParameter parameter : element.getParameterList().getParameters()) {
-      paramSB.setLength(0);
-      if (parameter.isVarArgs()) {
-        paramSB.append("...");
-      }
-      paramSB.append(parameter.getName());
-      paramSB.append(": ");
-      paramSB.append(TypeHelper.getParameterType(parameter));
-      params.add(paramSB.toString());
-    }
-    ctx.append(joiner.join(params));
-    ctx.append(')');
-
-    if (!element.isConstructor()) {
-      ctx.append(": ");
-
-      String returnType = TypeHelper.getMethodReturnType(element);
-      ctx.append(returnType);
-    }
-
-    PsiClass containingClass = (PsiClass) element.getParent();
-    if (!containingClass.isInterface()){
-      ctx.append(" {\n");
-
-      ctx.increaseIdent();
-      if (modifierList.hasModifierProperty("abstract") || element.getBody() == null) {
-        ctx.print("throw \"Abstract method\";\n");
-      } else {
-        element.getBody().accept(visitor);
-      }
-      ctx.decreaseIdent();
-
-      ctx.print("}\n");
-    } else {
-      ctx.append(";\n");
-    }
-    ctx.append("\n");
-  }
 
 }
