1dcfe8bb6cada7f0f837d8510861b74d6edacac9
Thomas Hartmann <thomi.hartmann@gmail.com>
2014-11-03 17:36:58 +0100

    fixed ModelCompareTest to test the union semantic: union on element level but merge/overrite semantic on attribute/reference level

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/microframework/test/CompareTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/microframework/test/CompareTest.java
@@ -5,10 +5,7 @@ import org.kevoree.modeling.api.Callback;
 import org.kevoree.modeling.api.data.MemoryKDataBase;
 import org.kevoree.modeling.api.trace.ModelTraceApplicator;
 import org.kevoree.modeling.api.trace.TraceSequence;
-import org.kevoree.modeling.microframework.test.cloud.CloudDimension;
-import org.kevoree.modeling.microframework.test.cloud.CloudUniverse;
-import org.kevoree.modeling.microframework.test.cloud.CloudView;
-import org.kevoree.modeling.microframework.test.cloud.Node;
+import org.kevoree.modeling.microframework.test.cloud.*;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +14,10 @@ import static org.junit.Assert.*;
  */
 public class CompareTest {
 
+    /**
+     * diff semantic: differences between two models,
+     * apply the diff trace to construct a model from another model
+     */
     @Test
     public void diffTest() {
         CloudUniverse universe = new CloudUniverse(new MemoryKDataBase());
@@ -71,6 +72,9 @@ public class CompareTest {
         });
     }
 
+    /**
+     * intersection semantic: simialarities between two models
+     */
     @Test
     public void intersectionTest() {
         CloudUniverse universe = new CloudUniverse(new MemoryKDataBase());
@@ -116,8 +120,11 @@ public class CompareTest {
         });
     }
 
+    /**
+     * union semantic: union but merge for same attributes/refs
+     */
     @Test
-    public void mergeTest() {
+    public void unionTest() {
         CloudUniverse universe = new CloudUniverse(new MemoryKDataBase());
         universe.newDimension(new Callback<CloudDimension>() {
             @Override
@@ -130,16 +137,34 @@ public class CompareTest {
                 // create two nodes
                 final Node node0_0 = t0.createNode();
                 node0_0.setName("node0_0");
+                node0_0.setValue("0_0");
+                final Element elem0_0 = t0.createElement();
+                elem0_0.setName("elem0_0");
+                node0_0.setElement(elem0_0);
 
                 final Node node0_1 = t0.createNode();
+                node0_1.setName("node0_1");
                 node0_1.setValue("0_1");
 
-                // test diff
+                // test union
                 t0.createModelCompare().union(node0_0, node0_1, new Callback<TraceSequence>() {
                     @Override
                     public void on(TraceSequence traceSequence) {
-                        assertEquals("{\"type\":\"SET\",\"src\":\"1\",\"meta\":\"name\",\"val\":\"node0_0\"}", traceSequence.traces()[0].toString());
-                        assertEquals("{\"type\":\"SET\",\"src\":\"1\",\"meta\":\"value\",\"val\":\"0_1\"}", traceSequence.traces()[1].toString());
+                        assertEquals("{\"type\":\"SET\",\"src\":\"1\",\"meta\":\"name\",\"val\":\"node0_1\"}", traceSequence.traces()[0].toString()); // merge behaviour
+                        assertEquals("{\"type\":\"SET\",\"src\":\"1\",\"meta\":\"value\",\"val\":\"0_1\"}", traceSequence.traces()[1].toString()); // merge behaviour
+                        assertEquals("{\"type\":\"ADD\",\"src\":\"1\",\"meta\":\"element\"}", traceSequence.traces()[2].toString()); // union behaviour
+
+                        new ModelTraceApplicator(node0_0).applyTraceSequence(traceSequence, new Callback<Throwable>() {
+                            @Override
+                            public void on(Throwable throwable) {
+                                node0_0.getElement(new Callback<Element>() {
+                                    @Override
+                                    public void on(Element element) {
+                                        assertEquals(elem0_0, element); // union behaviour
+                                    }
+                                });
+                            }
+                        });
                     }
                 });
 
