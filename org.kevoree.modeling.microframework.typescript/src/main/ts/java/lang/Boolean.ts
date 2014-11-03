b5b82821723baaef77a548ffec062e40f512ea03
Francois Fouquet <fouquet.f@gmail.com>
2014-11-03 10:00:46 +0100

    cleanup

--- org.kevoree.modeling.microframework.typescript/src/main/ts/java/lang/Boolean.ts
+++ org.kevoree.modeling.microframework.typescript/src/main/ts/java/lang/Boolean.ts
@@ -1,9 +1,5 @@
 class Boolean {
-    public static parseBoolean(val : string) : boolean {
-        if(val == "true"){
-            return true;
-        } else {
-            return false;
-        }
+    public static parseBoolean(val:string):boolean {
+        return val == "true";
     }
 }
