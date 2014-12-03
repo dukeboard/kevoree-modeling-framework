b9651a47e4abc1b5328fea70090e0df2bde547ac
Francois Fouquet <fouquet.f@gmail.com>
2014-12-03 09:37:53 +0100

    f

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/data/KeyCalculator.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/data/KeyCalculator.java
@@ -10,24 +10,23 @@ public class KeyCalculator {
     // Limit limit local index to LONG limit - prefix size
     public static final long INDEX_LIMIT = 0x0000001FFFFFFFFFl;
 
-
     private long _prefix;
     private long _currentIndex;
 
     public KeyCalculator(short prefix, long currentIndex) {
-        this._prefix = ((long)prefix) << 53-16;
+        this._prefix = ((long) prefix) << 53 - 16;
         this._currentIndex = currentIndex;
     }
 
     public long nextKey() {
-        if(_currentIndex == INDEX_LIMIT) {
+        if (_currentIndex == INDEX_LIMIT) {
             throw new IndexOutOfBoundsException("Object Index could not be created because it exceeded the capacity of the current prefix. Ask for a new prefix.");
         }
-        _currentIndex ++;
+        _currentIndex++;
         //moves the prefix 53-size(short) times to the left;
         long objectKey = _prefix + _currentIndex;
 
-        if(objectKey > LONG_LIMIT_JS) {
+        if (objectKey > LONG_LIMIT_JS) {
             throw new IndexOutOfBoundsException("Object Index exceeds teh maximum JavaScript number capacity. (2^53)");
         }
         return objectKey;
@@ -39,7 +38,7 @@ public class KeyCalculator {
     }
 
     public short prefix() {
-        return (short)(_prefix >> 53-16);
+        return (short) (_prefix >> 53 - 16);
     }
 
 }
