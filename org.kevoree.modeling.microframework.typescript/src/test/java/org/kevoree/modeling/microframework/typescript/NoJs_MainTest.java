0971f7fa068c386b299a074f6bd73b213bb2b7ad
Grégory NAIN <gregory.nain@gmail.com>
2014-12-16 18:46:18 +0100

    testrunner update

--- org.kevoree.modeling.microframework.typescript/src/test/java/org/kevoree/modeling/microframework/typescript/NoJs_MainTest.java
+++ org.kevoree.modeling.microframework.typescript/src/test/java/org/kevoree/modeling/microframework/typescript/NoJs_MainTest.java
@@ -1,5 +1,12 @@
 package org.kevoree.modeling.microframework.typescript;
 
+import de.flapdoodle.embed.nodejs.NodejsConfig;
+import de.flapdoodle.embed.nodejs.NodejsExecutable;
+import de.flapdoodle.embed.nodejs.NodejsProcess;
+import de.flapdoodle.embed.nodejs.NodejsRuntimeConfigBuilder;
+import de.flapdoodle.embed.nodejs.NodejsStarter;
+import de.flapdoodle.embed.nodejs.NodejsVersion;
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,52 +17,39 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Locale;
 
 /**
  * Created by gregory.nain on 28/11/14.
  */
 public class NoJs_MainTest {
-    public enum OSType {
-        Windows, MacOS, Linux, Other
-    }
-
-    public static OSType getOS() {
-        String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
-        if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
-            return OSType.MacOS;
-        } else if (OS.indexOf("win") >= 0) {
-            return OSType.Windows;
-        } else if (OS.indexOf("nux") >= 0) {
-            return OSType.Linux;
-        } else {
-            return OSType.Other;
-        }
-    }
 
     private static int launchRunner(String file) {
-        String[] params = new String[2];
 
-        if (getOS().equals(OSType.Windows)) {
-            params[0] = "node.exe";
-        } else {
-            params[0] = "node";
-        }
+        IRuntimeConfig runtimeConfig = (new NodejsRuntimeConfigBuilder()).defaults().build();
+        NodejsProcess node = null;
 
-        params[1] = NoJs_MainTest.class.getClassLoader().getResource(file).getFile();
-
-        ProcessBuilder pb = new ProcessBuilder(params);
-        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
         try {
-            Process p = pb.start();
-            int res = p.waitFor();
-            //Thread.sleep(200);
-            return res;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            
+            String basePath = NoJs_MainTest.class.getClassLoader().getResource(file).getFile();
+
+            NodejsConfig nodejsConfig = new NodejsConfig(NodejsVersion.Main.V0_10, basePath, new ArrayList<String>(), basePath.toString().substring(0, basePath.toString().lastIndexOf("/")));
+            NodejsStarter runtime = new NodejsStarter(runtimeConfig);
+
+            NodejsExecutable e = (NodejsExecutable) runtime.prepare(nodejsConfig);
+            node = (NodejsProcess) e.start();
+            return node.waitFor();
+        } catch (InterruptedException var11) {
+            var11.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (node != null) {
+                node.stop();
+            }
         }
         return -1;
     }
@@ -65,5 +59,9 @@ public class NoJs_MainTest {
     public void baseTest() {
         Assert.assertEquals(0,launchRunner("MyTestRunner.js"));
     }
+
+
+
+
 
 }
