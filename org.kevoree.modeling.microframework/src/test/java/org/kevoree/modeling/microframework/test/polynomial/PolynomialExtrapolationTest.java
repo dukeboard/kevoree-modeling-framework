9ee13301154cfbc315ced4a9011c2b940ceb0d16
Francois Fouquet <fouquet.f@gmail.com>
2014-12-22 17:16:41 +0100

    fix

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/microframework/test/polynomial/PolynomialExtrapolationTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/microframework/test/polynomial/PolynomialExtrapolationTest.java
@@ -13,12 +13,12 @@ public class PolynomialExtrapolationTest {
     @Test
     public void test() {
         int toleratedError = 5;
-        SimplePolynomialModel pe = new SimplePolynomialModel(0, toleratedError, 10, 1, Prioritization.LOWDEGREES);
+        SimplePolynomialModel pe = new SimplePolynomialModel(0, toleratedError, 10, Prioritization.LOWDEGREES);
         final double[] val = new double[1000];
         double[] coef = {2, 2, 3};
         for (int i = 200; i < 1000; i++) {
             long temp = 1;
-            val[i]=0;
+            val[i] = 0;
             for (int j = 0; j < coef.length; j++) {
                 val[i] = val[i] + coef[j] * temp;
                 temp = temp * i;
