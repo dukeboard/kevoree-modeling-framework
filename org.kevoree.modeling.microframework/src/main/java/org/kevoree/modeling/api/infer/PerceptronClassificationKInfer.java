75c81f2b87b219bf0c0c3efdd59158e3b23bd7f0
Assaad Moawad <assaad.moawad@uni.lu>
2015-02-11 10:29:03 +0100

    Adding Perceptron classification

---
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/api/infer/PerceptronClassificationKInfer.java
@@ -0,0 +1,129 @@
+package org.kevoree.modeling.api.infer;
+
+import org.kevoree.modeling.api.Callback;
+import org.kevoree.modeling.api.KInferState;
+import org.kevoree.modeling.api.KView;
+import org.kevoree.modeling.api.abs.AbstractKObjectInfer;
+import org.kevoree.modeling.api.infer.states.DoubleArrayKInferState;
+import org.kevoree.modeling.api.meta.MetaClass;
+import org.kevoree.modeling.api.time.TimeTree;
+
+/**
+ * Created by assaad on 10/02/15.
+ */
+public class PerceptronClassificationKInfer extends AbstractKObjectInfer {
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(double alpha) {
+        this.alpha = alpha;
+    }
+
+    public int getIterations() {
+        return iterations;
+    }
+
+    public void setIterations(int iterations) {
+        this.iterations = iterations;
+    }
+
+    /**
+     * @param alpha is the learning rate of the linear regression
+     */
+    private double alpha=0.0001;
+
+    /**
+     * @param iterations is the number of passes of the live learning on the training set
+     */
+    private int iterations=100;
+
+
+
+    public PerceptronClassificationKInfer(KView p_view, long p_uuid, TimeTree p_timeTree, MetaClass p_metaClass) {
+        super(p_view, p_uuid, p_timeTree, p_metaClass);
+    }
+
+    private double calculate(double[] weights, double[] features) {
+        double result=0;
+        for(int i=0;i<features.length;i++){
+            result+=weights[i]*features[i];
+        }
+        result+=weights[features.length];
+        return result;
+    }
+
+
+
+
+    @Override
+    public void train(Object[][] trainingSet, Object[] expectedResultSet, Callback<Throwable> callback) {
+        DoubleArrayKInferState currentState = (DoubleArrayKInferState) modifyState();
+        double[] weights=currentState.getWeights();
+
+        int featuresize=trainingSet[0].length;
+
+        if(weights==null){
+            weights=new double[featuresize+1];
+          /*  Random random = new Random();
+            for(int i=0; i<size+1;i++){
+                weights[i]=random.nextDouble();
+            }*/
+        }
+
+        double[][] features=new double[trainingSet.length][];
+        double[] results = new double[expectedResultSet.length];
+
+        for(int i=0;i<trainingSet.length;i++){
+            features[i] = new double[featuresize];
+            for(int j=0;j<featuresize;j++){
+                features[i][j]=(double) trainingSet[i][j];
+            }
+            results[i]=(double) expectedResultSet[i];
+        }
+
+        for(int j=0; j<iterations;j++) {
+            for(int i=0;i<trainingSet.length;i++){
+                double h = calculate(weights, features[i]);
+                double err = -alpha * (h - results[i]);
+                for (int k = 0; k < featuresize; k++) {
+                    weights[k] = weights[k] + err * features[i][k];
+                }
+                weights[featuresize] = weights[featuresize] + err;
+            }
+
+        }
+
+        currentState.setWeights(weights);
+    }
+
+    @Override
+    public Object infer(Object[] features) {
+        DoubleArrayKInferState currentState = (DoubleArrayKInferState) readOnlyState();
+        double[] weights=currentState.getWeights();
+        double[] ft=new double[features.length];
+        for(int i=0;i<features.length;i++){
+            ft[i]=(double)features[i];
+        }
+        return calculate(weights,ft);
+
+
+    }
+
+    @Override
+    public Object accuracy(Object[][] testSet, Object[] expectedResultSet) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        DoubleArrayKInferState currentState = (DoubleArrayKInferState) modifyState();
+        currentState.setWeights(null);
+    }
+
+    @Override
+    public KInferState createEmptyState() {
+        return new DoubleArrayKInferState();
+    }
+}
